d8080403ad94cf2617867c5f4b51f10eae3dce54
ETBox <13318972468@126.com>
2019-03-11 15:05:32 +0800

    task1

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true
+	},
+	'extends': 'airbnb-base',
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parserOptions': {
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'rules': {
+		"no-tabs": 0,
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
+}
