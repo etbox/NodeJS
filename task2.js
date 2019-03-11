47ef7d06abb708807586d039ff155165559728f1
ETBox <13318972468@126.com>
2019-03-11 15:39:40 +0800

    koa

--- task2.js
+++ task2.js
@@ -2,7 +2,29 @@ const Koa = require('koa')
 
 const app = new Koa()
 
+// logger
+
+app.use(async (ctx, next) => {
+	// console.log('logger')
+	await next()
+	const rt = ctx.response.get('X-Response-Time')
+	console.log(`${ctx.method} ${ctx.url} - ${rt}`)
+})
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+	// console.log('x-response-time')
+	const start = Date.now()
+	await next()
+	const ms = Date.now() - start
+	ctx.set('X-Response-Time', `${ms}ms`)
+})
+
+// response
+
 app.use(async (ctx) => {
+	// console.log('response')
 	ctx.body = 'Hello World'
 })
 
