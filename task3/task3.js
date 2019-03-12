14ac58681366bc08cb5ae5787ce1578d46925d5f
ETBox <13318972468@126.com>
2019-03-12 17:58:02 +0800

    feat: assign port in commander

--- task3/task3.js
+++ task3/task3.js
@@ -1,42 +1,46 @@
+#! /usr/bin/env node
+
 const Koa = require('koa')
 const Router = require('koa-router')
 const redis = require('redis')
 const {
 	promisify,
 } = require('util')
+const program = require('commander')
 
 const app = new Koa()
 const router = new Router()
 const client = redis.createClient()
-const getAsync = promisify(client.get).bind(client)
+const getAsync = promisify(client.get).bind(client) // 使用promise取得redis的数据
 
-client.on('connect', () => {
-	console.log('connected!')
+client.on('connect', () => { // 提示已连接redis
+	console.log('connected to Redis!')
 })
 
-// let getCtx
-// client.set('number', '')
+// let getCtx // 用于判断ctx的指向
+
 router.get('/', (ctx, next) => {
 	ctx.body = 'Hello World'
 }).get('/start', (ctx, next) => {
 	ctx.body = 'OK'
 
-	client.set('number', Math.floor(Math.random() * 100))
+	const num = Math.floor(Math.random() * 100)
+	client.set('number', num, () => {
+		console.log(`random number: ${num}`)
+	})
 }).get('/:number', async (ctx, next) => {
-	const clientNum = Number(ctx.params.number)
+	const clientNum = Number(ctx.params.number) // 请求favicon.ico时也会触发
 	console.log(`clientNum: ${clientNum}`)
 
 	// getCtx = ctx
 
 	await next()
 
-	console.log(`serverNum: ${ctx.serverNum}`)
 	const serverNum = Number(ctx.serverNum)
 
-
-	if (serverNum === null) {
+	if (ctx.serverNum === null) { // number未设置的情况
 		ctx.body = '<h1 style="text-align: center">please enter /start first</h1>'
-	} else if (serverNum === undefined) {
+	} else if (ctx.serverNum === undefined) { // redis炸了需要重启
 		ctx.body = '<h1 style="text-align: center">please shutdown Redis and relaunch it</h1>'
 	} else if (clientNum < serverNum) {
 		ctx.body = 'smaller'
@@ -47,11 +51,19 @@ router.get('/', (ctx, next) => {
 	}
 })
 
+program
+	.option('-p, --port <n>', '端口号', parseInt) // 使用命令行指定服务器监听的端口号
+	.parse(process.argv)
+
+console.log('port: %j', program.port)
+if (!program.port) {
+	program.port = 3000
+}
 
 app
 	.use(router.routes())
 	.use(router.allowedMethods())
-	.use(async (ctx) => {
+	.use(async (ctx) => { // 异步获取数据，并将数据保存在ctx中
 		async function getNumber() {
 			const serverNum = await getAsync('number')
 			ctx.serverNum = serverNum
@@ -61,4 +73,4 @@ app
 		await getNumber()
 		// console.log(`judge ${ctx === getCtx}`)
 	})
-	.listen(3000)
+	.listen(program.port)
