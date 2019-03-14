7a817a5695096b98312d80aaec171fdba248ee74
ETBox <13318972468@126.com>
2019-03-14 11:17:57 +0800

    refactor(async): use loop instead of function

--- task5/task5-client-async.js
+++ task5/task5-client-async.js
@@ -1,41 +1,39 @@
 const rp = require('request-promise')
 
 const url = 'http://localhost:3000'
-let guessNum = 500000
 let floor = 0
 let ceil = 1000000
-
-async function sendRequest() {
-	const body = await rp({
-		uri: `${url}/${guessNum}`,
-	})
-
-	console.log('body:', body)
-	if (body === 'smaller') {
-		floor = guessNum
-		guessNum = Math.floor((guessNum + ceil) / 2)
-		return sendRequest()
-	}
-	if (body === 'bigger') {
-		ceil = guessNum
-		guessNum = Math.floor((guessNum + floor) / 2)
-		return sendRequest()
-	}
-	if (body === 'equal') {
-		console.log(`Bingo! The number is ${guessNum}`)
-		return null
-	}
-	throw Error
-}
+let guessNum = Math.floor((floor + ceil) / 2)
 
 async function main() {
-	const body = await rp({
+	let body = await rp({
 		uri: `${url}/start`,
 	})
 
 	// Process html...
 	console.log('body:', body)
-	sendRequest()
+	if (body.search(/^[A-Za-z]+$/) === -1) {
+		throw Error(body)
+	}
+
+	do {
+		body = await rp({ // 使用循环才需要改变全局变量，因为不会产生局部变量
+			uri: `${url}/${guessNum}`,
+		})
+
+		console.log('body:', body)
+
+		if (body === 'smaller') {
+			floor = guessNum
+			guessNum = Math.floor((guessNum + ceil) / 2)
+		}
+		if (body === 'bigger') {
+			ceil = guessNum
+			guessNum = Math.floor((guessNum + floor) / 2)
+		}
+	} while (body !== 'equal')
+
+	console.log(`Bingo! The number is ${guessNum}`)
 }
 
 main()
