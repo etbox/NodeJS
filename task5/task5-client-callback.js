fb905ca7083ebd4c92d7d1e5a2ec70c0e0e8159d
ETBox <13318972468@126.com>
2019-03-14 10:24:27 +0800

    refactor(callback): pass parameters to sendRequest

--- task5/task5-client-callback.js
+++ task5/task5-client-callback.js
@@ -1,26 +1,26 @@
 const request = require('request')
 
 const url = 'http://localhost:3000'
-let guessNum = 500000
-let floor = 0
-let ceil = 1000000
+const floor = 0
+const ceil = 1000000
+const guessNum = Math.floor((floor + ceil) / 2)
 
-function sendRequest() {
+function sendRequest(num, flr, cel) {
 	request({
 		baseUrl: url,
-		url: `/${guessNum}`,
+		url: `/${num}`,
 	}, (error, response, body) => {
 		console.log('body:', body) // Print the HTML for the homepage.
 		if (body === 'smaller') {
-			floor = guessNum
-			guessNum = Math.floor((guessNum + ceil) / 2)
-			sendRequest()
+			// flr = num
+			// num = Math.floor((num + cel) / 2)
+			sendRequest(Math.floor((num + cel) / 2), num, cel)
 		} else if (body === 'bigger') {
-			ceil = guessNum
-			guessNum = Math.floor((guessNum + floor) / 2)
-			sendRequest()
+			// cel = num
+			// num = Math.floor((num + flr) / 2)
+			sendRequest(Math.floor((num + flr) / 2), flr, num)
 		} else if (body === 'equal') {
-			console.log(`Bingo! The number is ${guessNum}`)
+			console.log(`Bingo! The number is ${num}`)
 		} else {
 			throw Error
 		}
@@ -34,5 +34,5 @@ request({
 	console.log('error:', error) // Print the error if one occurred
 	console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
 	console.log('body:', body) // Print the HTML for the homepage.
-	sendRequest()
+	sendRequest(guessNum, floor, ceil)
 })
