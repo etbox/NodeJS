530f0e8e4ad3adecba730ed3080815c9f3e8e5b6
ETBox <13318972468@126.com>
2019-03-15 17:50:20 +0800

    refactor(async): use recursion instead of loop

--- task5/task5-client-async.js
+++ task5/task5-client-async.js
@@ -2,35 +2,38 @@ const rp = require('request-promise')
 
 const url = 'http://localhost:3000'
 
+async function sendRequest(floor, ceil) {
+	const guessNum = Math.floor((floor + ceil) / 2)
+	const body = await rp({
+		uri: `${url}/${guessNum}`,
+	})
+
+	console.log(`Guess body: ${body}`)
+	if (body === 'smaller') {
+		return sendRequest(guessNum, ceil)
+	}
+	if (body === 'bigger') {
+		return sendRequest(floor, guessNum)
+	}
+	if (body === 'equal') {
+		return guessNum
+	}
+	throw Error(body)
+}
+
 async function asyncMain(floor, ceil) {
-	let body = await rp({ // await会把错误向外层抛，故不用专门处理
+	const body = await rp({ // await会把错误向外层抛，故不用专门处理
 		uri: `${url}/start`,
 	})
 
-	console.log('body:', body)
-	if (body.search(/^[A-Za-z]+$/) === -1) {
+	console.log(`Request body: ${body}`)
+	if (body && body.search(/^[A-Za-z]+$/) === -1) { // 如果不返回一个单词就说明游戏没正常启动
 		throw Error(body)
+	} else if (!body) {
+		throw Error('Please modify your URL')
 	}
 
-	let guessNum = Math.floor((floor + ceil) / 2)
-	do {
-		body = await rp({
-			uri: `${url}/${guessNum}`,
-		})
-		console.log('body:', body)
-
-		if (body === 'smaller') {
-			floor = guessNum
-			guessNum = Math.floor((guessNum + ceil) / 2)
-		} else if (body === 'bigger') {
-			ceil = guessNum
-			guessNum = Math.floor((guessNum + floor) / 2)
-		} else if (body !== 'equal') {
-			throw Error(body)
-		}
-	} while (body !== 'equal')
-
-	return guessNum
+	return sendRequest(floor, ceil)
 }
 
 asyncMain(0, 1000000)
--- task5/task5-server.js
+++ task5/task5-server.js
@@ -44,7 +44,8 @@ router.get('/', (ctx, next) => {
 	} else if (clientNum > serverNum) {
 		ctx.body = 'bigger'
 	} else if (clientNum === serverNum) {
-		ctx.body = 'equal!' // 故意写多感叹号以报错
+		ctx.body = 'equal'
+		// ctx.body = 'equal!' // 故意写多感叹号以报错
 	}
 })
 
