8748a800546c80457756b1b13e58c13f1de5be30
ETBox <13318972468@126.com>
2019-04-18 11:37:05 +0800

    refactor: prettier eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,25 @@
 module.exports = {
-	'env': {
-		'browser': true,
-		'es6': true,
-		'node': true,
-		'mocha': true
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+		mocha: true,
 	},
-	'extends': 'airbnb-base',
-	'globals': {
-		'Atomics': 'readonly',
-		'SharedArrayBuffer': 'readonly'
+	extends: 'airbnb-base',
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
 	},
-	'parserOptions': {
-		'ecmaVersion': 2018,
-		'sourceType': 'module'
+	parserOptions: {
+		ecmaVersion: 2018,
+		sourceType: 'module',
 	},
-	'rules': {
-		"no-tabs": 0,
-		'indent': [
-			'error',
-			'tab'
-		],
-		'linebreak-style': [
-			'error',
-			'unix'
-		],
-		'quotes': [
-			'error',
-			'single'
-		],
-		'semi': [
-			'error',
-			'never'
-		],
-		"no-console": "off",
-	}
+	rules: {
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'no-tabs': 0,
+		'no-console': 'off',
+	},
 }
